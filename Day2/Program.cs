260b28450a99143af6f9f2c29cb23da59b208c2d
Lateesha <Lateesha@BEATMACHINE>
2017-01-17 12:04:49 -0500

    soap vs gromm

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -11,6 +11,31 @@ namespace Day2
     {
         static void Main(string[] args)
         {   //Class is the instructions or blueprint for object
+            //DogStuff();
+            Warrior gromm = new Warrior("Grommish");
+            Warrior soap = new Warrior("Soap McTavish");
+
+            soap.Attack(gromm);
+            Console.WriteLine(soap);
+            Console.WriteLine(gromm);
+            soap.Attack(gromm);
+
+            gromm.Attack(soap);
+
+            soap.Attack(gromm);
+            soap.Attack(gromm);
+            Console.WriteLine(soap);
+            Console.WriteLine(gromm);
+
+            {
+
+            }
+
+
+        }
+
+        private static void DogStuff()
+        {
             var fido = new Dog("Fido");              // have to have new there first method jumps to constructor setting age to 0
             fido.HappyBirthday();              // telling fido to have a birthday ( jumps to the 2nd and have age change)
             fido.HappyBirthday();              // telling fido to have a birthday ( jumps to the 2nd and have age change)
@@ -22,14 +47,14 @@ namespace Day2
             var ageDogYears = fido.GetAgeDogYears();
             Console.WriteLine(age);
             Console.WriteLine(ageDogYears);
-           // Console.WriteLine(fido.name);
+            // Console.WriteLine(fido.name);
 
             var casey = new Dog("Casey");
             var spaghetti = new Dog("Spaghetti");
             spaghetti.HappyBirthday();
             var rex = new Dog("Rex");
 
-           // Console.WriteLine(fido);
+            // Console.WriteLine(fido);
             fido.AddFriend(casey);
             fido.AddFriend(spaghetti);
             fido.AddFriend(rex);
@@ -42,9 +67,6 @@ namespace Day2
             fido.RemoveFriend(spaghetti);
 
             Console.ReadLine();
-
-
-
         }
     }
 }
---
+++ Day2/Warrior.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    enum State
+    {
+        Alive, Dead, Graveyard
+    }
+
+    class Warrior
+    {
+        private int hp;
+        public string name;
+        public State currentState = State.Alive;
+
+        public Warrior(string _name)
+        {
+            name = _name;
+            hp = 10;
+        }
+
+
+        public void LowerHP()
+        {
+            if (hp <= 3)
+            {
+                Console.WriteLine($"{name} is Dead. Can't be attacked");
+                hp = 0;
+                currentState = State.Dead; 
+            }
+            else
+            {
+                hp -= 3;
+            }
+        }
+        public void Attack(Warrior enemy)
+        {
+            Console.WriteLine($"{name} attacked {enemy.name}!");
+            enemy.LowerHP();
+        }
+
+        public override string ToString()
+        {
+            return $"{name} : {hp}";
+        }
+    }
+}
